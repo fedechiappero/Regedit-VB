6f4ebc106b60ea21d44276e89fb19b0ff02f0559
federico <fedechiappero06@gmail.com>
2020-01-04 22:18:56 -0300

    Add create multiline value
    Clean up code, no need to check system architecture

--- Regedit/Form1.vb
+++ Regedit/Form1.vb
@@ -6,34 +6,24 @@ Public Class Form1
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
-        'check system architecture
-        Dim baseKey
-        If Environment.Is64BitOperatingSystem Then '64 bits system
-            baseKey = Microsoft.Win32.RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, Microsoft.Win32.RegistryView.Registry64)
-        Else '32 bits system
-            baseKey = Microsoft.Win32.RegistryKey.OpenBaseKey(Microsoft.Win32.RegistryHive.LocalMachine, Microsoft.Win32.RegistryView.Registry32)
-        End If
         'hhd serial = registry entry to be found
         Dim hhd As New ManagementObject("win32_physicalmedia='\\.\physicaldrive0'")
         Dim serial = hhd.Properties("serialnumber").Value.ToString
-        Dim readValue As Object
+
         txt_serial.Text = serial
 
-        ' read registry entry and check his value
-        readValue = baseKey.OpenSubKey("SOFTWARE\Test")
-        Try '
-            If readValue.GetValue(serial.Trim) Is Nothing Then
+        Try
+            If Registry.LocalMachine.OpenSubKey("SOFTWARE\OrgName\SoftName") Is Nothing Then ' registry key not found
                 Dim msg = InputBox("Serial input...", "Provide a serial", Nothing)
-                baseKey.OpenSubKey("SOFTWARE", True).OpenSubKey("Test", True).CreateSubKey(msg.Trim).SetValue(msg.Trim, "test")
-                My.Computer.Registry.SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\Test", msg.Trim, "test")
-                'Dim regKey As RegistryKey
-                'regKey = Registry.LocalMachine.OpenSubKey("SOFTWARE", True).OpenSubKey("Test", True)
-                'regKey.SetValue(serial.Trim, "fake value")
-                'Dim writeValue = baseKey.CreateSubKey("SOFTWARE\Test", serial.Trim, "fake value")
-                'writeValue.SetValue(serial.Trim)
-            Else
-                MsgBox("readed value: " + readValue.GetValue(serial.Trim))
+                If msg.Trim = serial.Trim Then
+                    Registry.LocalMachine.OpenSubKey("SOFTWARE", True).CreateSubKey("OrgName\SoftName")
+                    Dim valores() As String = {}
+                    Registry.SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\OrgName\SoftName", serial.Trim, valores, Microsoft.Win32.RegistryValueKind.MultiString)
+                Else
+                    MsgBox("Invalid serial")
+                End If
             End If
+
         Catch ex As Exception
             MessageBox.Show(ex.Message)
         End Try
