ebf2a81a8895a6d379f809b8530f4da07feb565f
federico <fedechiappero06@gmail.com>
2020-01-04 23:04:40 -0300

    Add write values to key

--- Regedit/Form1.Designer.vb
+++ Regedit/Form1.Designer.vb
@@ -23,20 +23,42 @@ Partial Class Form1
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
         Me.txt_serial = New System.Windows.Forms.TextBox()
+        Me.txt_values = New System.Windows.Forms.TextBox()
+        Me.btn_save = New System.Windows.Forms.Button()
         Me.SuspendLayout()
         '
         'txt_serial
         '
-        Me.txt_serial.Location = New System.Drawing.Point(79, 116)
+        Me.txt_serial.Location = New System.Drawing.Point(79, 35)
         Me.txt_serial.Name = "txt_serial"
         Me.txt_serial.Size = New System.Drawing.Size(100, 20)
         Me.txt_serial.TabIndex = 0
+        '
+        'txt_values
+        '
+        Me.txt_values.Location = New System.Drawing.Point(79, 103)
+        Me.txt_values.Multiline = True
+        Me.txt_values.Name = "txt_values"
+        Me.txt_values.Size = New System.Drawing.Size(124, 67)
+        Me.txt_values.TabIndex = 1
+        Me.txt_values.Text = "efeAohDObmFTRx5" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "2YeDBKeOCeqVpHs" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "3y5BHI3XGB0adEU" & Global.Microsoft.VisualBasic.ChrW(13) & Global.Microsoft.VisualBasic.ChrW(10) & "RO5HpPd8y10960q"
+        '
+        'btn_save
+        '
+        Me.btn_save.Location = New System.Drawing.Point(104, 193)
+        Me.btn_save.Name = "btn_save"
+        Me.btn_save.Size = New System.Drawing.Size(75, 23)
+        Me.btn_save.TabIndex = 2
+        Me.btn_save.Text = "Save"
+        Me.btn_save.UseVisualStyleBackColor = True
         '
         'Form1
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(284, 261)
+        Me.Controls.Add(Me.btn_save)
+        Me.Controls.Add(Me.txt_values)
         Me.Controls.Add(Me.txt_serial)
         Me.Name = "Form1"
         Me.Text = "Form1"
@@ -45,5 +67,7 @@ Partial Class Form1
 
     End Sub
     Friend WithEvents txt_serial As System.Windows.Forms.TextBox
+    Friend WithEvents txt_values As System.Windows.Forms.TextBox
+    Friend WithEvents btn_save As System.Windows.Forms.Button
 
 End Class
--- Regedit/Form1.vb
+++ Regedit/Form1.vb
@@ -4,11 +4,13 @@ Imports Microsoft.Win32
 
 Public Class Form1
 
+    Dim serial
+
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
 
         'hhd serial = registry entry to be found
         Dim hhd As New ManagementObject("win32_physicalmedia='\\.\physicaldrive0'")
-        Dim serial = hhd.Properties("serialnumber").Value.ToString
+        serial = hhd.Properties("serialnumber").Value.ToString
 
         txt_serial.Text = serial
 
@@ -17,8 +19,8 @@ Public Class Form1
                 Dim msg = InputBox("Serial input...", "Provide a serial", Nothing)
                 If msg.Trim = serial.Trim Then
                     Registry.LocalMachine.OpenSubKey("SOFTWARE", True).CreateSubKey("OrgName\SoftName")
-                    Dim valores() As String = {}
-                    Registry.SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\OrgName\SoftName", serial.Trim, valores, Microsoft.Win32.RegistryValueKind.MultiString)
+                    Dim values() As String = {}
+                    Registry.SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\OrgName\SoftName", serial.Trim, values, Microsoft.Win32.RegistryValueKind.MultiString)
                 Else
                     MsgBox("Invalid serial")
                 End If
@@ -29,5 +31,27 @@ Public Class Form1
         End Try
 
 
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles btn_save.Click
+        Try
+            Dim values() As String = {}
+            Dim i As Integer = 0
+            For Each line As String In Me.txt_values.Lines
+                If line <> "" Then
+                    ReDim Preserve values(i)
+                    values(i) = line.Trim
+                    i += 1
+                End If
+            Next line
+
+            If values.Length > 0 Then
+                Registry.SetValue("HKEY_LOCAL_MACHINE\SOFTWARE\OrgName\SoftName", serial.Trim, values, Microsoft.Win32.RegistryValueKind.MultiString)
+            Else
+                MsgBox("No values to save")
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
     End Sub
 End Class
